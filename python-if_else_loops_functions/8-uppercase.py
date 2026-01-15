39e4ccdc7929f75332e1e9c7cf177718e750f398
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-15 10:36:49 +0100

    fix: var name inside function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     total = ""
     for i in str:
         if 97 <= ord(i) <= 122:
-            total = total + chr(ord(str) - 32)
+            total = total + chr(ord(i) - 32)
         else:
             total = total + i
     print("{}".format(total))
