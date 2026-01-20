b73a6d9fae55f7616d0c47075371232fa0c29f4b
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-20 10:44:32 +0100

    feat: function that replaces an element in a list at a specific position without modifying the original list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list_clone = my_list.copy
+    if idx >= len(my_list) or idx < 0:
+        return my_list_clone
+    else:
+        my_list[idx] = element
+        return my_list
