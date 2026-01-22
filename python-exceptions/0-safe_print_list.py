81ceeac36b9652538ed05a1de07e9052256b5892
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-22 12:31:10 +0100

    fix: increment length variable in safe_print_list function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
+            lenght += 1
         print()
     except IndexError:
         print()
