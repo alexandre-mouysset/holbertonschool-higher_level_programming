d2a96166f537731119d430bceab06599e70114fe
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-22 12:26:59 +0100

    feat: function that prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    lenght = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+    except IndexError:
+        print()
+
+    return lenght
