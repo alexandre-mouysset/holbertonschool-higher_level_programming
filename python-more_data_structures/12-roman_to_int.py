e5acbe90f8f2c789aac00d67789c7b3cd48a7e50
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-22 10:57:38 +0100

    fix: correct type checking condition in roman_to_int function

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str):
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
 
     Roman_Number = {"I": 1, "V": 5, "X": 10, "L": 50,
