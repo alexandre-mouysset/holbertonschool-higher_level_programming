ab46a97b773d34e10181a401bbf245a532e8f655
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-23 14:19:17 +0100

    fix: remove unnecessary blank line in say_my_name function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -15,7 +15,7 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If first_name or last_name is not a string
     """
-    
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
