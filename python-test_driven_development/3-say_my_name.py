29210f58c04a6b75a26250cd01132850748c8bbb
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-23 14:19:03 +0100

    removed commentary

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -15,12 +15,10 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If first_name or last_name is not a string
     """
-
-    # Check if first_name is a string
+    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    # Check if last_name is a string
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
