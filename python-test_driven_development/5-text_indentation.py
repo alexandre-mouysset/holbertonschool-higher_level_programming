b4d17fe93281bcb58111bcdb0ac9741dd6fb001b
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-23 14:41:02 +0100

    feat: function that prints a text with 2 new lines after each of certain character

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module for text indentation.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints text with 2 new lines after each '.', '?', ':'.
+
+    Args:
+        text: String to format
+
+    Raises:
+        TypeError: If text is not a string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    line = ""
+
+    for i in text:
+        line = line + i
+        if i == "." or i == "?" or i == ":":
+            print(line)
+            print()
+            line = ""
+    if line:
+        print(line)
