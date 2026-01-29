afdff28ba381551dc5a2c3c5034f3cb1be28d9b7
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-29 11:32:08 +0100

    feat: new class rectangke

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the Rectangle class for manipulating rectangles.
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle with its properties and methods.
+    """
+    pass
