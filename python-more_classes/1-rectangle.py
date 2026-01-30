6b8b5825484f8541c5ed79c7b610fd8702eeb8f8
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-30 16:57:11 +0100

    feat: class that defines a rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Rectangle class module."""
+
+
+class Rectangle:
+    """A rectangle with width and height attributes."""
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle with width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Return the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, width_value):
+        """Set width (must be a positive integer)."""
+        if not isinstance(width_value, int):
+            raise TypeError("width must be an integer")
+
+        if width_value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = width_value
+
+    @property
+    def height(self):
+        """Return the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, height_value):
+        """Set height (must be a positive integer)."""
+        if not isinstance(height_value, int):
+            raise TypeError("height must be an integer")
+
+        if height_value < 0:
+            raise TypeError("height must be >= 0")
+
+        self.__height = height_value
