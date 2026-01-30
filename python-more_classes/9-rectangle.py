31852b8a35a92c2dc69820f8a4efebfa4bb003e7
alexandre-mouysset <12258@holbertonstudents.com>
2026-01-30 20:16:12 +0100

    feat: add validation for width and height in Rectangle class

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -6,8 +6,16 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
