cade6d7353b26cf6a7ba9a3e2af0174336825486
alexandre-mouysset <12258@holbertonstudents.com>
2026-02-04 16:28:29 +0100

    feat: new class rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""BaseGeometry class module."""
+
+
+class BaseGeometry:
+    """A BaseGeometry class with area method."""
+
+    def area(self):
+        """Raise an exception indicating area is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer.
+
+        Args:
+            name: The name of the parameter (for error messages).
+            value: The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not greater than 0.
+        """
+        if not isinstance(value, int) or type(value) is bool:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
