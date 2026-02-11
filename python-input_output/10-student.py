f1047bc3c34132d824e571c21db96fcc960d3eee
alexandre-mouysset <12258@holbertonstudents.com>
2026-02-11 09:55:34 +0100

    feat: students function

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 """
-Module used to enhance the Student class with selective JSON serialization
+Module used to define a Student class and serialize it to JSON
 """
 
-Student = __import__("9-student").Student
+
+class Student:
+    """
+    Represents a student with basic information
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
 
 def to_json(self, attrs=None):
