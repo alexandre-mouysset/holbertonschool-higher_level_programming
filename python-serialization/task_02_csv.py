963ff3474f44f1c4bd29ddad888dde831d902f97
alexandre-mouysset <12258@holbertonstudents.com>
2026-02-13 10:47:20 +0100

    fix: typo

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,15 +4,15 @@ import json
 
 
 def convert_csv_to_json(csv_file):
-    list = []
+    data_list = []
     try:
         with open(csv_file, "r", encoding="UTF-8") as csv_filename:
             csv_Data = csv.DictReader(csv_filename)
             for row in csv_Data:
-                list.append(row)
+                data_list.append(row)
 
-        with open("data.jason", "w", encoding="UTF-8") as csv_filename:
-            json.dump(list, csv_filename)
+        with open("data.json", "w", encoding="UTF-8") as csv_filename:
+            json.dump(data_list, csv_filename)
             return True
 
     except FileNotFoundError:
