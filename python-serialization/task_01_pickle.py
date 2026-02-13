4f06fc0f046a125c6df7392d3b7e2d77b9fcd3d7
alexandre-mouysset <12258@holbertonstudents.com>
2026-02-13 10:28:05 +0100

    feat: documentation

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,19 +1,54 @@
 #!/usr/bin/python3
+"""
+Module for pickling and unpickling custom objects
+"""
 import pickle
 
 
 class CustomObjet:
+    """
+    A custom class for demonstrating pickle serialization
+
+    Attributes:
+        name (str): The name of the person
+        age (int): The age of the person
+        is_student (bool): Whether the person is a student
+    """
+
     def __init__(self, name, age, is_student):
+        """
+        Initializes a CustomObjet instance
+
+        Args:
+            name (str): The name of the person
+            age (int): The age of the person
+            is_student (bool): Whether the person is a student
+        """
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
+        """
+        Displays the object's attributes to stdout
+
+        Returns:
+            None
+        """
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
+        """
+        Serializes the object to a file using pickle
+
+        Args:
+            filename (str): Path to the file to save
+
+        Returns:
+            None
+        """
         try:
             with open(filename, "wb") as f:
                 pickle.dump(self, f)
@@ -22,6 +57,15 @@ class CustomObjet:
 
     @classmethod
     def deserialize(cls, filename):
+        """
+        Deserializes an object from a pickle file
+
+        Args:
+            filename (str): Path to the pickle file to load
+
+        Returns:
+            CustomObjet: The deserialized object, or None if an error occurs
+        """
         try:
             with open(filename, "rb") as f:
                 return pickle.load(f)
