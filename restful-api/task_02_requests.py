3a0adc4826e1194f8f5cc1dfb07d7dde7cffc85d
alexandre-mouysset <12258@holbertonstudents.com>
2026-02-18 20:53:24 +0100

    feat: Refactor fetch_and_print_posts and fetch_and_save_posts for improved readability

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,27 +8,28 @@ def fetch_and_print_posts():
     page_response = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {page_response.status_code}")
 
-    data = page_response.json()
-    for post in data:
-        print(post["title"])
+    if page_response.status_code == 200:
+        data = page_response.json()
+        for post in data:
+            print(post["title"])
 
 
 def fetch_and_save_posts():
 
     page_response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {page_response.status_code}")
 
     if page_response.status_code == 200:
         data = page_response.json()
+        posts_list = []
 
-    posts_list = []
+        for post in data:
+            post_dic = {
+                "id": post["id"], "title": post["title"], "body": post["body"]
+            }
+            posts_list.append(post_dic)
 
-    for post in data:
-        post_dic = {
-            "id": post["id"], "title": post["title"], "body": post["body"]
-        }
-        posts_list.append(post_dic)
-
-    with open("posts.csv", "w", newline='', encoding="UTF-8") as f:
-        writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
-        writer.writeheader()
-        writer.writerows(posts_list)
+        with open("posts.csv", "w", newline='', encoding="UTF-8") as f:
+            writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts_list)
