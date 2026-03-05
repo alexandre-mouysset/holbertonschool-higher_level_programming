b678f1ec3b376eab5c75a25903fc2cec09f8ecae
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-05 14:55:52 +0100

    feat: script that create a database and a table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- use database
+USE hbtn_0d_usa;
+
+-- create table
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
