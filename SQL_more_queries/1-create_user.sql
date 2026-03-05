1c5dce5aacd3814af924cd67dbd75ad566e40d0f
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-05 13:49:26 +0100

    fix: typo

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create server user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES On *.* TO 'user_0d_1'@'localhost';
