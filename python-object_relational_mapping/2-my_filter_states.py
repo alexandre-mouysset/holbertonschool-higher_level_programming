4f10db2081375c7023bd48764d7df162eb733834
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-06 14:23:57 +0100

    update: documentation

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists states with a name starting with 'N' from the database"""
+"""Lists all states with a name matching the argument from the database"""
 import MySQLdb
 import sys
 
