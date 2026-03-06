d66d53a71d92016a060fa97d67e751036aa8e999
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-06 14:15:05 +0100

    added documentation

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Lists states with a name starting with 'N' from the database"""
 import MySQLdb
 import sys
 
 
 def main():
+    """Define the main function"""
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
