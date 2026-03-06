b496ef701f12d62585af117c39f793295f023521
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-06 10:02:47 +0100

    feat: script that list all the cities of california that can be found on the database

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- list all the cities of california that can be found on the database
+SELECT id, name 
+FROM cities
+WHERE state_id = (
+	SELECT id 
+	FROM states 
+	WHERE name = 'California'
+	);
+ORDER BY id ASC;
