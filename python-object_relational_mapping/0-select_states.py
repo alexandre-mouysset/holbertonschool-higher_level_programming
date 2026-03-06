0d1b1259b20907bf3438af8cead79d2a50cb5ccb
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-06 13:45:25 +0100

    feat: added all file needed + script that list all states from a database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Récupérer les arguments passés au script
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Se connecter à la base MySQL sur localhost:3306
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password, db=database
+        )
+
+    cursor = db.cursor()
+
+    # Exécuter la requête pour récupérer tous les états triés par id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    rows = cursor.fetchall()
+
+    # Afficher les résultats
+    for row in rows:
+        print(row)
+
+    # Fermer la connexion
+    cursor.close()
+    db.close()
