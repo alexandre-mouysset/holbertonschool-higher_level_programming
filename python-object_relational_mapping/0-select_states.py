54631165a2b4824faf5c492796b49e0442f9beab
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-06 13:54:01 +0100

    refactor: update docstring and improve variable naming for clarity

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-"""List all states from the database."""
-
+"""Lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 
 def main():
-    """Run the SQL query and print results."""
-    user = sys.argv[1]
+    """Define the main function"""
+    username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=user,
-        passwd=password,
-        db=database
-    )
-
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
     cursor = db.cursor()
-
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
     states = cursor.fetchall()
-
     for state in states:
         print(state)
-
     cursor.close()
     db.close()
 
