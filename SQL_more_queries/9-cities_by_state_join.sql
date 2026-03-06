08e3078789f4661f23e5c012330d5d95d07dbc9f
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-06 10:24:07 +0100

    fix: replace JOIN with INNER JOIN in cities by state query

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script that list all the cities contained in the database
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states_id
+INNER JOIN states ON cities.state_id = states_id
 ORDER BY state.id;
