e638adf55e83c9cdad3c8a11ff68c23bb384717f
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-06 14:25:20 +0100

    refactor: update docstring and streamline database connection code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states with a name matching the argument from the database"""
+"""Display all values in the states table."""
 import MySQLdb
 import sys
 
@@ -9,28 +9,15 @@ def main():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
+    searched = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
     cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        state_name)
-
-    cursor.execute(query)
-
+    cursor.execute("SELECT * FROM states WHERE name = '{}'"
+                   " ORDER BY id ASC".format(searched))
     states = cursor.fetchall()
-
     for state in states:
         print(state)
-
     cursor.close()
     db.close()
 
