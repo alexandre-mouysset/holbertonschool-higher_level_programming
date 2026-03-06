d905f0382ff5bea4d4f7c6520a379842815e49ac
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-06 10:20:04 +0100

    fix: correct join condition in cities by state query

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script that list all the cities contained in the database
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = state_id
+JOIN states ON cities.state_id = states_id
 ORDER BY cities.id ASC;
