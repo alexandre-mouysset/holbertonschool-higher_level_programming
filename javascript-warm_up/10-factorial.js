7b7ac911b072bcbf73d6612c19d3647e6c994c34
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-24 10:46:15 +0100

    fixed linter issue

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 function factorial (n) {
-	if (n <= 1) {
-		return 1;
-	}
+  if (n <= 1) {
+    return 1;
+  }
 
-	return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
 
 const n = parseInt(process.argv[2], 10);
