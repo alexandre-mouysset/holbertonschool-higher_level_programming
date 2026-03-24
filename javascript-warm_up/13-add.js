861aa44e3b60b9123ffea1c03720bce4b181a71b
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-24 13:08:06 +0100

    style: format code for consistency

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function add (a, b) {
-	if (isNaN(a) || isNaN(b)) {
-		return 'Not a number';
-	}  else {
-		return a + b;
-	}
+  if (isNaN(a) || isNaN(b)) {
+    return 'Not a number';
+  } else {
+    return a + b;
+  }
 }
-module.exports = {add};
+module.exports = { add };
