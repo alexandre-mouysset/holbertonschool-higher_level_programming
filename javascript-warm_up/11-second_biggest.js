dd73731f4ecbb222e997bd8a4a50a5ad1aedb683
alexandre-mouysset <12258@holbertonstudents.com>
2026-03-24 12:06:23 +0100

    feat: implement logic to find the second largest number from command line arguments

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2).map(arg => parseInt(arg));
+let result;
+
+if (args.length < 2) {
+  result = 0;
+} else {
+  let first = -Infinity;
+  let second = -Infinity;
+
+  for (const n of args) {
+    if (n > first) {
+      second = first;
+      first = n;
+    } else if (n > second && n < first) {
+      second = n;
+    }
+  }
+  if (second === -Infinity) {
+    result = 0;
+  } else {
+    result = second;
+  }
+}
+console.log(result);
